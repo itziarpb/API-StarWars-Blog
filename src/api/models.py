07684b44d40053bab651633834e304c98368a4d1
itziarpb <itziar.perezb@gmail.com>
2022-12-08 23:29:20 +0000

    modelos

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +15,40 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "email": self.email
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(120))
+    hair_color = db.Column(db.String(80))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.gender,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.String(120))
+    clime = db.Column(db.String(80))
+    
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    user = db.relationship(User)
+    character = db.relationship(Character)
+
+class FavoritePlanet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    user = db.relationship(User)
+    planet = db.relationship(Planet)
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,22 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet, FavoriteCharacter, FavoritePlanet
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/user', methods=['GET'])
+def list_user():
+    user = User.query.all()
+    data =[user.serialize() for user in users]
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    return jsonify(data), 200
 
-    return jsonify(response_body), 200
+@app.route('/character', methods=['GET'])
+def list_character():
+    character = Character.query.all()
+    data =[character.serialize() for character in characters]
+
+    return jsonify(data), 200
